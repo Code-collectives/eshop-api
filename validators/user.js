9ee93961b41f5ed11e1a99893fcfdf08d7fdd564
Amanda Mensah-Biney <airlinepilotamanda@gmail.com>
2024-10-23 15:59:07 +0000

    done by force!

--- validators/user.js
+++ validators/user.js
@@ -2,15 +2,15 @@ import Joi from "joi";
 
 export const registerUserValidator = Joi.object({
     name: Joi.string().required(),
-    email: Joi.string().required().unique(),
-    password: Joi.number().strict().required(),
-    role: Joi.string().required(),
+    email: Joi.string().email().required(),
+    password: Joi.string(),
+    role: Joi.string().valid('user', 'vendor'),
    
 
 });
 
 export const signInUserValidator = Joi.object({
-    email: Joi.string().required().unique(),
-    password: Joi.number().strict().required()
+    email: Joi.string().email().required(),
+    password: Joi.string()
 
 });
