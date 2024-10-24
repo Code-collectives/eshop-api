2c4dd81845bd57f85fc369e53b30a648e0fc3c35
Gideon Odoom <gidodoom@gmail.com>
2024-10-24 15:03:18 +0000

    getting users ads only

--- controllers/user.js
+++ controllers/user.js
@@ -1,98 +1,118 @@
-import { signInUserValidator, registerUserValidator} from "../validators/user.js";
+import { signInUserValidator, registerUserValidator } from "../validators/user.js";
 import { UserModel } from "../models/user.js";
+import { AdvertModel } from "../models/advert.js";
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
-import { mailtransporter} from "../utils/mail.js"
+import { mailtransporter } from "../utils/mail.js"
 
 
 
 
 export const registerUser = async (req, res, next) => {
   try {
-const {error, value} = registerUserValidator.validate(req.body)
-if (error) {
-  return res.status(422).json(error)
-}
-const user = await UserModel.findOne({ email: value.email})
-if (user) {
-  res.status(409).json('User already exists')
-}
-const hashedPassword = bcrypt.hashSync(value.password, 10);
-await UserModel.create({
-  ...value,
-  password: hashedPassword
-});
+    const { error, value } = registerUserValidator.validate(req.body)
+    if (error) {
+      return res.status(422).json(error)
+    }
+    const user = await UserModel.findOne({ email: value.email })
+    if (user) {
+      res.status(409).json('User already exists')
+    }
+    const hashedPassword = bcrypt.hashSync(value.password, 10);
+    await UserModel.create({
+      ...value,
+      password: hashedPassword
+    });
 
-await mailtransporter.sendMail({
-  to: value.email,
-  subject: 'USER REGISTRATION',
-  text: 'Account successfully registered!'
-})
+    await mailtransporter.sendMail({
+      to: value.email,
+      subject: 'USER REGISTRATION',
+      text: 'Account successfully registered!'
+    })
 
-res.status(200).json('User successfully registered!')
-  }catch(error){
+    res.status(200).json('User successfully registered!')
+  } catch (error) {
     next(error)
   }
 }
 
 
-export const signInUser = async (req, res, next) =>{
-  try{
-    const { error, value} = signInUserValidator.validate(req.body)
-    if(error){
+export const signInUser = async (req, res, next) => {
+  try {
+    const { error, value } = signInUserValidator.validate(req.body)
+    if (error) {
       return res.status(422).json(error)
     }
-const user = await UserModel.findOne({ email: value.email})
-if (!user){
-  return res.status(404).json('User does not exist')
-}
-const correctPassword = bcrypt.compareSync(value.password, user.password)
-if (!correctPassword) {
-  return res.status(404).json('Invalid credentials!')
-}
-const token = jwt.sign(
-  {id: user.id},
-  process.env.JWT_PRIVATE_KEY,
-  { expiresIn: '24h'}
-)
-res.json({
-  message: 'User signed in!',
-  accessToken: token
-})
+    const user = await UserModel.findOne({ email: value.email })
+    if (!user) {
+      return res.status(404).json('User does not exist')
+    }
+    const correctPassword = bcrypt.compareSync(value.password, user.password)
+    if (!correctPassword) {
+      return res.status(404).json('Invalid credentials!')
+    }
+    const token = jwt.sign(
+      { id: user.id },
+      process.env.JWT_PRIVATE_KEY,
+      { expiresIn: '24h' }
+    )
+    res.json({
+      message: 'User signed in!',
+      accessToken: token
+    })
 
 
-} catch (error) {
-  next(error)
+  } catch (error) {
+    next(error)
 
   }
 }
 
 
 
-export const getProfile = async( req, res, next) =>{
-  try{
+export const getProfile = async (req, res, next) => {
+  try {
     const user = await UserModel
-    .findById(req.auth.id)
-    .select ({ password: false});
+      .findById(req.auth.id)
+      .select({ password: false });
     res.json(user);
-  }catch (error){
+  } catch (error) {
     next(error)
   };
 }
 
 
 export const updateProfile = async (req, res, next) => {
-  try{
-const { error, value} = updateProfileValidator.validate({
-  ...req.body,
-});
-if(error){
-  return res.status(422).json(error);
-}
+  try {
+    const { error, value } = updateProfileValidator.validate({
+      ...req.body,
+    });
+    if (error) {
+      return res.status(422).json(error);
+    }
 
-await UserModel.findByIdAndUpdate(req.auth.id, value);
-res.json('User profile updated!')
-  }catch(error){
+    await UserModel.findByIdAndUpdate(req.auth.id, value);
+    res.json('User profile updated!')
+  } catch (error) {
     next(error);
   }
+}
+
+export const getUserAdverts = async (req, res, next) => {
+  try {
+    const { filter = "{}", limit = 10, skip = 0, sort = "{}" } = req.query;
+    const adverts = await AdvertModel
+      .find({
+        ...JSON.parse(filter),
+        user: req.auth.id
+      })
+      .sort(JSON.parse(sort))
+      .limit(limit)
+      .skip(skip);
+    res.status(200).json(adverts);
+  } catch (error) {
+    next(error)
+
+  }
+
 }
