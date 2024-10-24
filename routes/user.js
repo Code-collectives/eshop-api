7990b485e1f0ef370734d90e17adaa2b07aff37f
Gideon Odoom <gidodoom@gmail.com>
2024-10-24 15:13:47 +0000

    get userprofile ads only

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 
 
 import express from 'express';
-import { getProfile, registerUser, signInUser, updateProfile } from '../controllers/user.js';
+import { getProfile, getUserAdverts, registerUser, signInUser, updateProfile } from '../controllers/user.js';
 import { hasPermission, isAuthenticated } from '../middlewares/auth.js';
 
 const userRouter = express.Router();
@@ -9,8 +9,13 @@ const userRouter = express.Router();
 // User routes
 
 userRouter.get( '/users/me', isAuthenticated, hasPermission('get_profile'), getProfile);
+
+userRouter.get('/users/me/adverts', isAuthenticated, getUserAdverts);
+
 userRouter.patch( '/users/me', isAuthenticated, hasPermission('update_profile'), updateProfile)
+
 userRouter.post('/users/register', registerUser);
+
 userRouter.post('/users/signIn', signInUser);
 
 
